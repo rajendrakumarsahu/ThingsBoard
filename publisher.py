373e24699879cafd5e4df9587501d6d920164394
rajendrakumarsahu <rajendrakumarsahu1986@gmail.com>
2021-11-23 23:21:39 +0530

    Add files via upload

--- publisher.py
+++ publisher.py
@@ -15,16 +15,51 @@ def on_message(client, userdata, msg):
       print("Message received:"+str(msg.payload.decode("utf-8")))
       print("Message topic :"+str(msg.topic))
 client = paho.Client("MQTT")
-sensor_data = {'temperature': 0, 'humidity': 0}
+sensor_data = {
+	"deviceName": "WESCO001000",
+	"deviceFriendlyName": "Smart Meter Sensor",
+	"deviceId": "784f394c-42b6-435a-983c-b7beff2784f9",
+	"totalPowerConsumed": 5500,
+	"topics": {
+		"eventTopic": {
+			"timeStamp": "Unix time Stamp",
+			"activePower": 9000,
+			"eventType": "High Active Power, Device Fault",
+			"deviceName": "WESCO001000",
+			"deviceId": "784f394c-42b6-435a-983c-b7beff2784f9",
+			"deviceFriendlyName": "Smart Meter Sensor"
+		},
+		"telemteryTopic": {
+			"timeStamp": "Unix time Stamp",
+			"activePower": 9000,
+			"reactivePower": 3000,
+			"deviceName": "WESCO001000",
+			"deviceId": "784f394c-42b6-435a-983c-b7beff2784f9",
+			"deviceFriendlyName": "Smart Meter Sensor"
+		},
+		"attributeTopic": {
+			"timeStamp": "Unix time Stamp",
+			"activePower": 9000,
+			"reactivePower": 3000,
+			"totalPowerConsumed": 2500,
+			"deviceName": "WESCO001000",
+			"deviceId": "784f394c-42b6-435a-983c-b7beff2784f9",
+			"deviceFriendlyName": "Smart Meter Sensor"
+		}
+	}
+}
 # Set access token
 client.username_pw_set(username="mqtt", password="rajendra")
 client.on_connect=on_connect
 client.connect("localhost", 1883)
-client.subscribe("api/mqtt")
 client.loop_start()
 while True:
     time.sleep(1)
-    sensor_data['temperature'] = random.randint(1, 100)
-    sensor_data['humidity'] = random.randint(1, 100)
-    client.publish('test', sensor_data['temperature'], 1)
+    sensor_data['totalPowerConsumed'] = random.randint(1, 1000)
+    sensor_data['topics']['eventTopic']['activePower'] = random.randint(1, 1000)
+    sensor_data['topics']['telemteryTopic']['activePower'] = random.randint(1, 1000)
+    sensor_data['topics']['telemteryTopic']['reactivePower'] = random.randint(1, 1000)
+    sensor_data['topics']['attributeTopic']['activePower'] = random.randint(1, 1000)
+    sensor_data['topics']['attributeTopic']['reactivePower'] = random.randint(1, 1000)
+    client.publish('api/mqtt/publish', json.dumps(sensor_data), 1)
     
